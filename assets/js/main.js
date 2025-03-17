c8c9a8adc72462470b37cba9b8b9bea24dbba0ad
shazee-04 <mgssrangajeewa@gmail.com>
2025-03-17 20:33:38 +0530

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-// scroll progress bar animations
+gsap.registerPlugin(ScrollTrigger, Draggable);
 
-gsap.registerPlugin(ScrollTrigger);
+// scroll progress bar animations
 gsap.to('progress', {
   value: 100,
   ease: 'none',
   scrollTrigger: { scrub: 0.5 }
+});
+
+//test
+window.addEventListener('scroll', () => {
+  document.getElementById('test').innerHTML = Math.floor(window.scrollY);
 });
